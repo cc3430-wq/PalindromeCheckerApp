e302c0f93c02f0195ce04437186bf3800846189a
chandu33 <cc3430@srmist.edu.in>
2026-02-25 23:43:49 +0530

    hi

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("welcome to palinedrome checker app");
+        System.out.println("version : 1.0");
+        System.out.println("System init successfully");
+    }
+}
